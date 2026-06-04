8cfe89b60c6acc7e5a88f5d61da6c3682a9f2dd9
aseelfouqha <aseel.abdulhalim@gmail.com>
2026-06-04 10:17:36 +0300

    Debuging

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./Pages/home/Home";
-import Users from "./Pages/Users/Users";
+import Users from "./Pages/users/users";
 
 
 
